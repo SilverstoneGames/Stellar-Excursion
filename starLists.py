2f14e2f3b836b5a72ecb49dd39cd8fd7b8fd10d0
Ulyn at Silverstone Games <35208190+SilverstoneGames@users.noreply.github.com>
2018-01-08 08:18:24 -0500

    Initial Files (#1)
    These files currently contain code relating to stellar generation, the procedural and random generation of new stars.

---
+++ starLists.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Star Lists.py
+Created on Sun Jan  7 19:44:37 2018
+
+@author: Shane
+"""
+
+starNames = ["Rigel", "Spica", "Sirius", ]
+starTypes = ["O", "B", "A", "F", "G", "K", "M"]
+starTypeNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
