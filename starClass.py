48b93d296dc069a35cf3945e5a672d01d24e6378
Ulyn at Silverstone Games <35208190+SilverstoneGames@users.noreply.github.com>
2018-01-08 12:16:08 -0500

    Finished the rough alpha of stellar generation
    This includes functionality to display the basic info for each star generated.

--- starClass.py
+++ starClass.py
@@ -6,16 +6,17 @@ Created on Sun Jan  7 19:44:36 2018
 
 @author: Shane
 """
-import numpy as n
+from numpy import random as n
 import random as r
-import StarLists.py as SL
+import starLists as SL
 class Star:
     
     def __init__(self, name):
         x = r.uniform(0, 1)
+        self.starName = name
         if x <= 0.00000001:
             self.starType = "Pulsar"
-        elif x > 0.00000001 && <= 0.000000025:
+        elif x > 0.00000001 and x <= 0.000000025:
             self.starType = "Neutron Star"
         else:
             self.starType = r.choice(SL.starTypes)
@@ -36,17 +37,17 @@ class Star:
             self.starMass = r.randint(2, 4)
             self.starLumin = r.randint(60, 80)
         elif self.starType == "F":
-            self.starColour = r.choice("White", "Blue", "Blue-white")
+            self.starColour = r.choice(SL.typeFcolours)
             self.starAppTemp = r.randint(6000, 7500)
             self.starMass = r.uniform(1.5, 1.9)
             self.starLumin = r.randint(4, 10)
         elif self.starType == "G":
-            self.starColour = r.choice("White", "Yellow-white", "Yellow")
+            self.starColour = r.choice(SL.typeGcolours)
             self.starAppTemp = r.randint(5000, 6000)
             self.starMass = r.uniform(1.0, 1.2)
             self.starLumin = r.uniform(0.8, 1.4)
         elif self.starType == "K":
-            self.starColour = r.choice("Orange", "Red-orange", "Red")
+            self.starColour = r.choice(SL.typeKcolours)
             self.starAppTemp = r.randint(3500, 5000)
             self.starMass = r.uniform(0.6, 0.9)
             self.starLumin = r.uniform(0.2, 0.5)
@@ -57,9 +58,10 @@ class Star:
             self.starLumin = r.uniform(0.2, 0.5)   
         
     def display(self):
-        print("Star Class: " + self.starType + self.starTypeNum)
-        print("Colour:     " + self.starColour)
-        print("Approximate Surface Temperature: " + self.starAppTemp)
-        print("Approximate Mass: " + self.starMass + " Solar Masses")
-        print("Approximate Luminosity: " self.starLumin + " times Sol luminosity")
+        print("Star Name:                       " + self.starName)
+        print("Star Class:                      " + str(self.starType) + str(self.starTypeNum))
+        print("Colour:                          " + self.starColour)
+        print("Approximate Surface Temperature: " + str(self.starAppTemp))
+        print("Approximate Mass:                " + str(self.starMass) + " Solar Masses")
+        print("Approximate Luminosity:          " + str(self.starLumin) + " times Sol luminosity")
         print("Press any key to exit...")
--- starLists.py
+++ starLists.py
@@ -6,6 +6,9 @@ Created on Sun Jan  7 19:44:37 2018
 @author: Shane
 """
 
-starNames = ["Rigel", "Spica", "Sirius", ]
+starNames = ["Rigel", "Spica", "Sirius"]
 starTypes = ["O", "B", "A", "F", "G", "K", "M"]
 starTypeNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+typeFcolours = ["White", "Blue", "Blue-white"]
+typeGcolours = ["White", "Yellow-white", "Yellow"]
+typeKcolours = ["Orange", "Red-orange", "Red"]
