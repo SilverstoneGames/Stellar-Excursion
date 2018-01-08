21187750305bed0489b7e96bfe5ffbf20be6151c
Ulyn at Silverstone Games <35208190+SilverstoneGames@users.noreply.github.com>
2018-01-08 11:39:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ Created on Sun Jan  7 19:43:04 2018
 @author: Shane
 """
 
+import StarLists.py as SL
+import StarClass.py as SC
+import random as r
+
+def main():
+  
+  star = SL.Star(r.choice(SL.starNames))
+  
+  star.display()
+  
+  print("Press enter to exit...")
