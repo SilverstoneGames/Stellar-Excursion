257ebfe71799ef8ec49eef1e1f40ef6ba914925f
Ulyn at Silverstone Games <35208190+SilverstoneGames@users.noreply.github.com>
2018-01-09 18:48:42 -0500

    Updated to allow generation of new star (#3)
    This is the basic form of travel in Stellar Excursion. It requires a lot of work yet, but this allows to effectively "travel" to a newly generated star.

--- starLists.py
+++ starLists.py
@@ -5,7 +5,7 @@ Created on Sun Jan  7 19:44:37 2018
 
 @author: Shane
 """
-
+exploredStars = open("exploredStars.txt", "a+")
 starNames = ["Rigel", "Spica", "Sirius"]
 starTypes = ["O", "B", "A", "F", "G", "K", "M"]
 starTypeNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
